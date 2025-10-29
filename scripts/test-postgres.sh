928a208b085cd67b722e7fdcddfed0814eb0435d
RhythmicCarnage <71204385+ProhibitedTV@users.noreply.github.com>
2025-10-29 11:53:21 -0600

    Allow postgres test script to use module proxy

--- scripts/test-postgres.sh
+++ scripts/test-postgres.sh
@@ -97,7 +97,7 @@ fi
 
 echo "running go test -tags postgres ${packages[*]}" >&2
 export GOTOOLCHAIN="${GOTOOLCHAIN:-local}"
-export GOPROXY="${GOPROXY:-https://proxy.golang.org,direct}"
-export GOSUMDB="${GOSUMDB:-sum.golang.org}"
+export GOPROXY="https://proxy.golang.org,direct"
+export GOSUMDB="sum.golang.org"
 
 go test -count=1 -tags postgres "${packages[@]}"
